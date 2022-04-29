457de02cdac50cd59b290fdd110b2037b9d64b70
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2022-04-28 21:06:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//Muda o nome a cada 5 minutos a partir das 9 da manhã
+//Muda o nome a cada 5 minutos
 
 client.on('ready', () => {
   const horario = new Date()
-  var i = 0
-  var nomes = [/*lista de nomes*/]
+  let i = 0
+  let nomes = [/*lista de nomes*/]
 
-if(horario.getHours() >= 9){
     const mudarNome = setInterval(() => {
       const servidor = client.guilds.cache.get(/*Id do servidor*/)
       const eu = servidor.members.fetch(/*Id do usuario*/)
@@ -18,11 +17,6 @@ if(horario.getHours() >= 9){
       i++
       if(i > nomes.length){i = 0}
   }, 300000)
-}
-
-  if(horario.getHours() >= 0 && horario.getHours() < 9){
-    clearInterval(mudarNome)
-  }
 
 })
 
